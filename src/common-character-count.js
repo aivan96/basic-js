6bda8511acfed236b326d74de80a6183b97f65ca
Ivan Shinkevich <55103473+aivan96@users.noreply.github.com>
2022-05-02 23:35:21 +0300

    Add files via upload

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let arr1 = s1.split("").sort();
+  let arr2 = s2.split("").sort();
+  let l1 = arr1.length; let l2 = arr2.length;
+  let lBig; let result;
+
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxDepth = 0, depth = 0;
+    arr.forEach(elem => {
+      if (Array.isArray(elem)) {
+        depth = this.calculateDepth(elem);
+      }
+      if (maxDepth < depth) maxDepth = depth;
+    });
+
+    return maxDepth + 1;
   }
 }
 
